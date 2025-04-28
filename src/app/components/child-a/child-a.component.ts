f82c71389acadd099df7927bebd97af154d59721
Roostertail Software <RoostertailSoftware@users.noreply.github.com>
2025-04-28 15:59:17 -0400

    playing around with if form validations

--- src/app/components/child-a/child-a.component.ts
+++ src/app/components/child-a/child-a.component.ts
@@ -12,20 +12,23 @@ import { ChildBComponent } from '../child-b/child-b.component';
 export class ChildAComponent implements OnInit {
   // If the form is changed I will inform this output
   childsOutput = output<FormGroup>();
-
+  JSON = JSON
   //My Form
   FormG!: FormGroup;
 
   // for giggles -- create as many childB components
   howManyChildB: Array<number> = [1, 2, 3, 4];
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {
+ 
+   }
 
   ngOnInit(): void {
     this.FormG = this.fb.group({
-      firstName: [null, [Validators.minLength(5), Validators.required]],
-      middleName:[null, [Validators.minLength(0) ]],
+      firstName: ['', [Validators.required, Validators.minLength(5) ]],
+      middleName:[null, [Validators.minLength(0), Validators.maxLength(1) ]],
     });
+
     // Send this to parent
     this.passChildOnToParent( this.FormG )
 
@@ -34,5 +37,20 @@ export class ChildAComponent implements OnInit {
   // Pass whatever comes in onto Parent
   passChildOnToParent(event: FormGroup) {
     this.childsOutput.emit(event);
+  }
+
+  isRequired( s: string ): boolean {
+    return this.FormG.controls[s].hasValidator( Validators.required)
+  }
+
+  isWorthy(s: string ):  boolean {
+    return this.FormG.controls['firstName'].invalid && 
+        ( this.FormG.controls['firstName'].dirty || 
+          this.FormG.controls['firstName'].touched )
+  }
+  isWorthy2( f: FormGroup ): boolean {
+    return f.invalid && 
+        ( f.dirty || 
+          f.touched )
   }
 }
